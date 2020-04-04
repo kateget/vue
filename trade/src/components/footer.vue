e531e06e0bc37ece0b339829e80a6ce62c249ebb
kateget <1397945742@qq.com>
2020-04-04 18:20:07 +0800

    foot head 样式添加

--- trade/src/components/footer.vue
+++ trade/src/components/footer.vue
@@ -1,9 +1,98 @@
 <template>
-    <div class="flooter">flooter</div>
+    <div class="flooter">
+        <ul >
+            <li>
+                走进中科
+                <ul class="company">
+                    <li >公司简介</li>
+                    <li >企业文化</li>
+                    <li >发展历程</li>
+                    <li >组织架构</li>
+                    <li >合作伙伴</li>
+                </ul>
+            </li>
+            <li>主营业务
+                <ul class="company2">
+                    <li>进口业务</li>
+                    <li>出口业务</li>
+                    <li>供应链业务</li>
+                    <li>代理招标业务</li>
+                    <li>离岸业务</li>
+                </ul>
+            </li>
+            <li>企业荣誉
+                <ul class="company3">
+                    <li>企业资质</li>
+                </ul>
+            </li>
+            <li>售后服务
+                <ul class="company4">
+                    <li>服务特色</li>
+                </ul>
+            </li>
+            <li>广东省中科进出口有限公司
+                <ul class="company5">
+                    <li>地址：广东省广州市先烈中路100号科学院9号一楼</li>
+                    <li>电话：+86(0)20-3765 6268</li>
+                    <li>传真：+86(0)20-8768 2371  +86(0)20-8768 1505</li>
+                    <li>网址：http://www.gdstie.com</li>
+                    <li>邮箱：shengz_k@gdstie.com</li>
+                    <li>邮编：510070</li>
+                </ul>
+            </li>
+        </ul>
+    </div>
 </template>
 <script>
 
 </script>
 <style>
-
+*{
+    margin: 0;
+}
+.flooter{
+    padding: 50px 0px;
+    margin-top: 10px;
+    background: #36383e;
+    color:#fff;
+    font-size: 16px;
+    display: inline-block;
+    width: 100%;
+}
+.company,.company2,.company3,.company4,.company5{
+    color:#ccc;
+    font-size: 14px;
+    padding: 0;
+    text-align: left;
+    line-height: 30px;
+    position: relative;
+}
+.company li,.company2 li,.company3 li,.company4 li{
+    position: relative;
+}
+.flooter > ul > li:last-child{
+    width: 400px;
+}
+.flooter > ul > li{
+    
+    width:180px;
+    float: left;
+    text-align: left;
+    line-height: 36px;
+}
+.flooter li{
+    list-style: none;
+}
+.flooter > ul{
+    display: block;
+    margin: 0 auto;
+    width: 1200px;
+}
+.company li::before,.company2 li::before,.company3 li::before,.company4 li::before{
+    display: block;
+    content: ".";
+    left: -10px;
+    line-height: 20px;
+    position: absolute;
+}
 </style>
--- trade/src/components/header.vue
+++ trade/src/components/header.vue
@@ -1,10 +1,41 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-04-04 17:22:10
+ * @LastEditTime: 2020-04-04 17:40:02
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \umid:\study\vue\vue\trade\src\components\header.vue
+ -->
 <template>
-    <div class="header">header</div>
+    <div class="header">
+        <img class="log" src="@/assets/logo.png" />
+        <div class="lang">
+            <img class="zh" src="@/assets/cn.png" />
+            <img class="en" src="@/assets/en.png" />
+        </div>
+    </div>
 </template>
 <script>
     export default{
     }
 </script>
-<style>
-
+<style >
+.header{
+    width:1200px;
+    margin: 0 auto;
+    height: 120px;
+    position: relative;
+}
+.lang{
+    position: absolute;
+    right:0;
+    top:50%;
+    transform: translateY(-50%);
+}
+.en{
+    margin-left: 10px;
+}
+.en,.zh{
+    cursor: pointer;
+}
 </style>
